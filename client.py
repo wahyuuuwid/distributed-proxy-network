aa4063fa5b1749e8382d38e494e536fe50dfdb40
Juanmeno25 <Jhirendy75@gmail.com>
2026-05-23 11:12:26 +0700

    fix: update client.py tanpa argumen

---
+++ client.py
@@ -0,0 +1,46 @@
+from socket import *
+
+BUFFER_SIZE = 4096
+
+# Default langsung tanpa argumen
+serverName = '127.0.0.1'
+serverPort = 8080
+filePath = '/index.html'
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# Koneksi ke server
+try:
+    print(f"Connecting to {serverName}:{serverPort}...")
+    clientSocket.connect((serverName, serverPort))
+except ConnectionRefusedError:
+    print(f"[ERROR] Tidak bisa konek ke {serverName}:{serverPort}. Pastikan server sudah berjalan.")
+    exit(1)
+
+# Kirim HTTP Request
+requestMessage = f"GET {filePath} HTTP/1.1\r\nHost: {serverName}\r\nConnection: close\r\n\r\n"
+clientSocket.send(requestMessage.encode())
+
+# Terima response
+response = b""
+while True:
+    data = clientSocket.recv(BUFFER_SIZE)
+    if not data:
+        break
+    response += data
+
+clientSocket.close()
+
+# Pisahkan header dan body
+print("\nResponse from server:\n")
+try:
+    header_end = response.index(b"\r\n\r\n")
+    header = response[:header_end].decode()
+    body = response[header_end + 4:].decode(errors='replace')
+
+    print("--- RESPONSE HEADER ---")
+    print(header)
+    print("\n--- RESPONSE BODY ---")
+    print(body)
+except ValueError:
+    print(response.decode(errors='replace'))
